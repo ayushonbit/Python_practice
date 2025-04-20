02cec804d74af69df362d86469ffdd5199c3d5d0
StealthDevAyush <ayushpandey762@gmail.com>
2025-04-20 11:58:37 +0530

    oops part

---
+++ OOPS/main_bank.py
@@ -0,0 +1,15 @@
+from bank_account import *
+
+
+SARA = bank_Account(2000,"SARA")
+LAUREN = bank_Account(3000,"Lauren")
+
+# SARA.deposit(1000)
+
+# SARA.getBalance()
+
+# # SARA.withdraw(5000)
+
+# SARA.withdraw(10)
+
+SARA.tansfer(100, LAUREN)
